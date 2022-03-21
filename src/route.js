fe3140bedbce6e56a20f1a2aedf1d14431248ef0
Alexandre Marra <alexsmarra@hotmail.com>
2022-03-21 18:13:15 -0300

    terminando javascript do form

--- src/route.js
+++ src/route.js
@@ -5,6 +5,16 @@ const route = express.Router()
 
 // response para renderizar o index.ejs, ou seja, o que virá após o localhost:3000/ 
 route.get('/', (req, res) => res.render("index"))
+// responses para renderizar as demais páginas
+route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/room', (req, res) => res.render("room"))
+
+// Formato que o formulário de dentro da modal tem que passar a informação
+// route.post('/room/:room/:question/:action')
 
 // exportando a const route
 module.exports = route
+
+
+
+
