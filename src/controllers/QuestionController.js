29d34f0ca5c7b75d4b2007bc3e93e43f400a333e
Alexandre Marra <alexsmarra@hotmail.com>
2022-03-25 12:09:53 -0300

    cadastrando questions no bd

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,3 +1,5 @@
+const Database = require('../db/config')
+
 module.exports = {
    index(req, res) {
       // pegando as requisições da linha 'route.post('/room/:room/:question/:action', QuestionController.index)' do  route.js, ou seja, pegando os valores de cada variável (valores de :room, :questionId e :action) 
@@ -8,5 +10,25 @@ module.exports = {
       const password = req.body.password
 
       console.log(`roomId: ${roomId}, questionId: ${questionId}, action: ${action}, password: ${password}`)
+   },
+
+   async create(req, res) {
+      const db = await Database()
+
+      const question = req.body.question
+      const roomId = req.params.room
+
+      // os valores de textos (string), mesmo sendo variáveis, precisam estar dentro de aspas duplas, como  "${question}"
+      await db.run(`INSERT INTO questions(
+         title,
+         room,
+         read
+      )VALUES(
+         "${question}",
+         ${roomId},
+         "0"
+      )`)
+
+      res.redirect(`/room/${roomId}`)
    }
 }
--- src/route.js
+++ src/route.js
@@ -8,15 +8,15 @@ const route = express.Router()
 // response para renderizar o index.ejs, ou seja, o que virá após o localhost:3000/ 
 // O page é uma variável para usarmos no index.ejs, para montar o html de forma mais eficiente (com menos código) e para aproveitar as partes em comum.
 route.get('/', (req, res) => res.render("index", { page: 'enter-room' }))
-// responses para renderizar as demais páginas
 route.get('/create-pass', (req, res) => res.render("index", { page: 'create-pass' }))
 
-route.get('/room/:room', (req, res) => res.render("room"))
+route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
 
+route.post('/question/create/:room', QuestionController.create)
 // Formato que o formulário de dentro da modal tem que passar a informação. As palavras após o : como o :room e o :question por exemplo, é a forma de colocarmos variáveis, é no lugar delas que entrarão os dados que pegamos dos elementos através do  main.js
 route.post('/question/:room/:question/:action', QuestionController.index)
 
-route.post('/create-room', RoomController.create)
 
 // exportando a const route
 module.exports = route
